5dd18a97ec075f0be99e580509be8ba319c979a1
Sawsan Masri <sawsan.msri@gmail.com>
2023-03-05 10:54:02 +0200

    Build design tree

---
+++ second-course/AGENDA.md
@@ -0,0 +1,15 @@
+# React Introduction
+
+# Thinking in React
+
+# Create React App
+
+# React files and folders
+
+# App.js play around
+
+# Creating and nesting components
+
+# React Dev Tools
+
+# Writing JSX
--- second-course/src/App.js
+++ second-course/src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
-function App() {
+const data = [1, 2, 3, 4, 5];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+    <MainContent />
+    <Footer />
+  )
+};
+
+export const Header = () => {
+  return (
+   <>
+    <HeaderLeft />
+    <HeaderRight />
+   </>
+  )
+}
+
+export const HeaderLeft = () => {
+  return (
+    <div>
+    <MainTitle />
+    <SubTitle />
     </div>
-  );
+  )
+}
+
+export const MainTitle = () => {
+  return (
+    <>
+      <h2>Main Title</h2>
+    </>
+  )
+}
+export const SubTitle = () => {
+  return (
+    <>
+      <h5>Sub Title</h5>
+    </>
+  )
+}
+
+export const HeaderRight = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export const Navigation = () => {
+  return (
+    <>
+      {
+        [1,2,3].map(item => <ListItem />)
+      }
+    </>
+  )
+}
+
+export const ListItem = () => {
+  return (
+      <a href="#">label</a> 
+  )
+}
+
+export const Footer = () => {
+  return (
+      <>
+        <MainTitle />
+        <p className="paragraph">Copy right</p>
+      </>
+  )
+}
+
+export const MainContent = () => {
+  return (
+      <>
+        <LeftSide />
+        <RightSide />
+      </>
+  )
+}
+
+export const LeftSide = () => {
+  return (
+      <>
+       <FeaturedPost />
+       {
+        [1,2,3].map(item=><RecentPost />)
+       }
+      </>
+  )
+}
+
+
+export const FeaturedPost = () => {
+  return (
+    <>
+      <img src = "#" />
+      <RecentDate />
+      <MainTitle />
+      <Paragraph />
+      <SubTitle />
+    </>
+  )
+}
+
+export const RecentPost = () => {
+  return (
+    <>
+      <div>
+<img src="" /> 
+<RecentDate />
+
+      </div>
+      <div>
+<MainTitle />
+<Paragraph />
+<SubTitle />
+      </div>
+    </>
+  )
+}
+
+export const RightSide = () => {
+  return (
+      <>
+       <AboutMe />
+       <RecentPosts />
+      </>
+  )
+}
+
+export const AboutMe = () => {
+  return (
+      <>
+       <MainTitle />
+       <img src="#" />
+       <Paragraph />
+      </>
+  )
+}
+
+export const RecentPosts = () => {
+  return (
+      <>
+       <MainTitle />
+       {
+        [1,2,3].map(item =><Post />)
+       }
+       
+      </>
+  )
+}
+
+export const Post = () => {
+  return (
+      <>
+      <img src="#" />
+      <p>Post data</p>
+      </>
+  )
 }
 
 export default App;
+
