0cd40dceeb48f8952ae1fbbdc026be842513540d
Sawsan Masri <sawsan.msri@gmail.com>
2023-03-20 17:31:24 +0200

    Installing modlues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
-import Carousel from "./components/Exercises/Carousel";
 import LeftColumn from "./components/LeftColumn";
 import RightColumn from "./components/RightColumn";
 import Shop from "./components/Shop";
-import Cart from "./components/Exercises/Cart";
 import Badge from "./components/Badge";
 import Label from "./components/Label";
 
@@ -21,4 +19,7 @@ export default App;
 // Keeping components pure
 //   It minds its own business. It does not change any objects or variables that existed before it was called.
 //   Same inputs, same output. Given the same inputs, a pure function should always return the same result.
-// Installing Modules -> https://github.com/brillout/awesome-react-components
+// Installing Modules -> https://github.com/brillout/awesome-react-components // button
+// exercise -> react-slick
+// Material UI
+// localStorage & sessionStorage
