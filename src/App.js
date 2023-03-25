dbe7a039a4fad59e9ff7b0ced31114eaa2d698de
Sawsan Masri <sawsan.msri@gmail.com>
2023-03-25 04:38:28 +0300

    Adding more state examples

--- src/App.js
+++ src/App.js
@@ -20,11 +20,13 @@ export default App;
 
 // Final Project preparation
 // Passing functions as props with parameters -> example1
-// Prevent Default / Stop Propagation -> exercise -> ex.2 & ex.3
+// Prevent Default / Stop Propagation
+// exercise -> ex.2 & ex.3
 // exercise -> install and use react-slick
 // State -> example4 ... etc
 // State is isolated -> ex9
 // Hooks—functions starting with use—can only be called at the top level of your components or your own Hooks.
 // You can’t call Hooks inside conditions, loops, or other nested functions
-// state exercise -> ex3
+// state exercise -> ex3,ex4
 // localStorage & sessionStorage & Cookies
+// Sign up & Login
