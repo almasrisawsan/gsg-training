08fceb901e69220c0a546a8bcd29ba86a6994d4b
Sawsan Masri <sawsan.msri@gmail.com>
2023-04-15 08:13:40 +0300

    Reset state

--- src/App.js
+++ src/App.js
@@ -1,44 +1,206 @@
 import Router from "./route";
 import { CookiesProvider } from "react-cookie";
 
-export default function App() {
+function App() {
+  return (
+    <CookiesProvider>
+      <Router />
+    </CookiesProvider>
+  );
+}
+
+export default App;
+// (Alternatively, you may hold the selected index in state.)
+//  * import { useState } from 'react';
+
+// const initialItems = [
+//   { title: 'pretzels', id: 0 },
+//   { title: 'crispy seaweed', id: 1 },
+//   { title: 'granola bar', id: 2 },
+// ];
+
+// export default function Menu() {
+//   const [items, setItems] = useState(initialItems);
+//   const [selectedId, setSelectedId] = useState(0);
+
+//   const selectedItem = items.find(item =>
+//     item.id === selectedId
+//   );
+
+//   function handleItemChange(id, e) {
+//     setItems(items.map(item => {
+//       if (item.id === id) {
+//         return {
+//           ...item,
+//           title: e.target.value,
+//         };
+//       } else {
+//         return item;
+//       }
+//     }));
+//   }
+
+//   return (
+//     <>
+//       <h2>What's your travel snack?</h2>
+//       <ul>
+//         {items.map((item, index) => (
+//           <li key={item.id}>
+//             <input
+//               value={item.title}
+//               onChange={e => {
+//                 handleItemChange(item.id, e)
+//               }}
+//             />
+//             {' '}
+//             <button onClick={() => {
+//               setSelectedId(item.id);
+//             }}>Choose</button>
+//           </li>
+//         ))}
+//       </ul>
+//       <p>You picked {selectedItem.title}.</p>
+//     </>
+//   );
+// }
+
+/**
+ * Notice how if you first click “Choose” on an item and then edit it,
+ *  the input updates but the label at the bottom does not reflect the edits.
+ * This is because you have duplicated state, and you forgot to update selectedItem.
+ */
+/**
+ * 
+ * import { useState } from 'react';
+
+const initialItems = [
+  { title: 'pretzels', id: 0 },
+  { title: 'crispy seaweed', id: 1 },
+  { title: 'granola bar', id: 2 },
+];
+
+export default function Menu() {
+  const [items, setItems] = useState(initialItems);
+  const [selectedItem, setSelectedItem] = useState(
+    items[0]
+  );
+
+  function handleItemChange(id, e) {
+    setItems(items.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title: e.target.value,
+        };
+      } else {
+        return item;
+      }
+    }));
+  }
+
   return (
     <>
-      <CookiesProvider>
-        <Router />
-      </CookiesProvider>
+      <h2>What's your travel snack?</h2> 
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <input
+              value={item.title}
+              onChange={e => {
+                handleItemChange(item.id, e)
+              }}
+            />
+            {' '}
+            <button onClick={() => {
+              setSelectedItem(item);
+            }}>Choose</button>
+          </li>
+        ))}
+      </ul>
+      <p>You picked {selectedItem.title}.</p>
     </>
   );
 }
 
-// Login example
-// Save token in cookies
-// Get authorized user data and adding headers in axios body
-// Axios baseUrl
-// Axios interceptors
-// Protected routes
+(Alternatively, you may hold the selected index in state.)
+ * import { useState } from 'react';
 
-// Reacting to input with state  -> exmp8, go to exp1 before
-// Exercises ->  ex5.js
+const initialItems = [
+  { title: 'pretzels', id: 0 },
+  { title: 'crispy seaweed', id: 1 },
+  { title: 'granola bar', id: 2 },
+];
+
+export default function Menu() {
+  const [items, setItems] = useState(initialItems);
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find(item =>
+    item.id === selectedId
+  );
+
+  function handleItemChange(id, e) {
+    setItems(items.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title: e.target.value,
+        };
+      } else {
+        return item;
+      }
+    }));
+  }
+
+  return (
+    <>
+      <h2>What's your travel snack?</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <input
+              value={item.title}
+              onChange={e => {
+                handleItemChange(item.id, e)
+              }}
+            />
+            {' '}
+            <button onClick={() => {
+              setSelectedId(item.id);
+            }}>Choose</button>
+          </li>
+        ))}
+      </ul>
+      <p>You picked {selectedItem.title}.</p>
+    </>
+  );
+}
+
+ */
+
+// Protected routes
+// exercise -> create 3 pages and router
+// How to use SASS to styles components -> variables, imports
+// Back to state
 // Choosing the state structure ->
-// -- Group related state -> exmp10
-// -- Avoid contradictions in state -> exmp11
-// -- Avoid redundant state -> exmp13
+// -- Group related state -> exmp10  -> done
+// -- Avoid contradictions in state -> exmp11 -> done
+// -- Avoid redundant state -> exmp13 -> done
 // -- Avoid duplication in state -> exmp14
-// -- Avoid deeply nested state -> exmp15
-// Exercises -> ex6.js, ex7.js, ex8.js
+// -- Avoid deeply nested state -> exmp15 --> will add it as an example on google classes
+// Exercises --> ex.6, ex7, ex8
+
 // Sharing state between components ->
-// -- Lifting state up -> exmp16 ----> check screenshots before and after
+// -- Lifting state up -> exmp16 ----> Done
 // Exercises -> ex9.js, ex10.js
 // Preserving and resetting state (using key) ->
-// -- Re rendering component resets the state -> exmp18.js
-// -- Same component at the same position preserves the state -> exmp19.js
-// -- Different component at the same position resets the state -> exmp20.js, exmp21.js
+// -- Re rendering component resets the state -> exmp18.js -> Done
+// -- Same component at the same position preserves the state -> exmp19.js -> Done
+// -- Different component at the same position resets the state -> exmp20.js, exmp21.js -> Done
 // -- Nesting components functions is not a good practice -> exmp22.js
 // -- Resetting the state at the same position -> exmp23.js
 // -- Resetting state with a key -> exmp24.js
 // -- Resetting state with a key -> exmp12.js
 // Exercises -> ex11.js, ex12.js, ex13.js, ex14.js
-// Installing Axios and using API's to submit a form
-// localStorage & sessionStorage & Cookies
-// Sign up & Login
+// React memo -> exmp25
+// React memo --> has to do with props but not it's own state --> exmp26
