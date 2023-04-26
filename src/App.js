d0cfc1b7c47120ed07199b6b4a4204d71c244312
Sawsan Masri <sawsan.msri@gmail.com>
2023-04-26 10:59:11 +0300

    Memo and useMemo

--- src/App.js
+++ src/App.js
@@ -11,29 +11,9 @@ function App() {
 
 export default App;
 
-// Protected routes
-// exercise -> create 3 pages and router -> Login , Sign Up, Home page
-// How to use SASS to styles components -> variables, imports
-// Back to state
-// Choosing the state structure ->
-// -- Group related state -> exmp10  -> done
-// -- Avoid contradictions in state -> exmp11 -> done
-// -- Avoid redundant state -> exmp13 -> done
-// -- Avoid duplication in state -> exmp14
-// -- Avoid deeply nested state -> exmp15 --> will add it as an example on google classes
-// Exercises --> ex.6, ex7, ex8
-
-// Sharing state between components ->
-// -- Lifting state up -> exmp16 ----> Done
-// Exercises -> ex9.js, ex10.js
-// Preserving and resetting state (using key) ->
-// -- Re rendering component resets the state -> exmp18.js -> Done
-// -- Same component at the same position preserves the state -> exmp19.js -> Done
-// -- Different component at the same position resets the state -> exmp20.js, exmp21.js -> Done
-// -- Nesting components functions is not a good practice -> exmp22.js
-// -- Resetting the state at the same position -> exmp23.js
-// -- Resetting state with a key -> exmp24.js
-// -- Resetting state with a key -> exmp12.js
-// Exercises -> ex11.js, ex12.js, ex13.js, ex14.js
-// React memo -> exmp25
-// React memo --> has to do with props but not it's own state --> exmp26
+// React memo -> exmp1
+// React memo --> has to do with props but not it's own state --> exmp2
+// New Hook -> useMemo
+// Usage:
+// Skipping expensive calculations -> exmp3
+// Skipping re-rendering with useMemo and memo -> exmp4
--- src/components/Examples/exmp2.js
+++ src/components/Examples/exmp2.js
@@ -1,31 +1,57 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 
-let initialArtists = [
-  { id: 0, name: "Marta Colvin Andrade" },
-  { id: 1, name: "Lamidi Olonade Fakeye" },
-  { id: 2, name: "Louise Nevelson" },
-];
-
-export default function List() {
-  const [artists, setArtists] = useState(initialArtists);
-
+export default function MyApp() {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
   return (
     <>
-      <h1>Inspiring sculptors:</h1>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>
-            {artist.name}{" "}
-            <button
-              onClick={() => {
-                // delete selected item (filter)
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <label>
+        Name{": "}
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Address{": "}
+        <input value={address} onChange={(e) => setAddress(e.target.value)} />
+      </label>
+      <Greeting name={name} />
+    </>
+  );
+}
+
+const Greeting = memo(function Greeting({ name }) {
+  console.log("Greeting was rendered at", new Date().toLocaleTimeString());
+  const [greeting, setGreeting] = useState("Hello");
+  return (
+    <>
+      <h3>
+        {greeting}
+        {name && ", "}
+        {name}!
+      </h3>
+      <GreetingSelector value={greeting} onChange={setGreeting} />
+    </>
+  );
+});
+
+function GreetingSelector({ value, onChange }) {
+  return (
+    <>
+      <label>
+        <input
+          type="radio"
+          checked={value === "Hello"}
+          onChange={(e) => onChange("Hello")}
+        />
+        Regular greeting
+      </label>
+      <label>
+        <input
+          type="radio"
+          checked={value === "Hello and welcome"}
+          onChange={(e) => onChange("Hello and welcome")}
+        />
+        Enthusiastic greeting
+      </label>
     </>
   );
 }
