3ae440035a04a2035967d52ba93c8514ce8830e1
Sawsan Masri <sawsan.msri@gmail.com>
2023-04-29 11:24:41 +0300

    Disable styles

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,48 +1,48 @@
-import { useCookies } from "react-cookie";
-import axios from "../../axios";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useCookies } from "react-cookie";
+// import axios from "../../axios";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-function User() {
-  const [cookies] = useCookies(["token"]);
-  const [user, setUser] = useState({});
-  const navigate = useNavigate();
+// function User() {
+//   const [cookies] = useCookies(["token"]);
+//   const [user, setUser] = useState({});
+//   const navigate = useNavigate();
 
-  const getUserData = async () => {
-    try {
-      const results = await axios.get("/auth/profile");
-      const {
-        data: { name, role },
-      } = results;
-      setUser({
-        name: name,
-        role: role,
-      });
-    } catch (error) {}
-  };
+//   const getUserData = async () => {
+//     try {
+//       const results = await axios.get("/auth/profile");
+//       const {
+//         data: { name, role },
+//       } = results;
+//       setUser({
+//         name: name,
+//         role: role,
+//       });
+//     } catch (error) {}
+//   };
 
-  const userLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
+//   const userLogout = () => {
+//     localStorage.removeItem("token");
+//     navigate("/login");
+//   };
 
-  const { name, role } = user;
-  return (
-    <div className={styles.mainWrapper}>
-      <div className={styles.mainContainer}>
-        <button className={styles.btn1} onClick={getUserData}>
-          Get user data
-        </button>
-        <h2 className={styles.name}>Name: {name}</h2>
-        <h2 className={styles.role}>Role: {role}</h2>
-        <h2>Hello</h2>
-        <button onClick={userLogout}>Logout</button>
-      </div>
-      <button className={styles.btn2} onClick={getUserData}>
-        Get user data
-      </button>
-    </div>
-  );
-}
+//   const { name, role } = user;
+//   return (
+//     <div className={styles.mainWrapper}>
+//       <div className={styles.mainContainer}>
+//         <button className={styles.btn1} onClick={getUserData}>
+//           Get user data
+//         </button>
+//         <h2 className={styles.name}>Name: {name}</h2>
+//         <h2 className={styles.role}>Role: {role}</h2>
+//         <h2>Hello</h2>
+//         <button onClick={userLogout}>Logout</button>
+//       </div>
+//       <button className={styles.btn2} onClick={getUserData}>
+//         Get user data
+//       </button>
+//     </div>
+//   );
+// }
 
-export default User;
+// export default User;
