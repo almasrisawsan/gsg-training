3b80012930e3bc351f011689719c3937a24a5b5c
Oways Asi <owayslfkf@gmail.com>
2023-05-02 18:09:28 +0300

    Solution of day 7

--- src/components/Exercises/ex1.js
+++ src/components/Exercises/ex1.js
@@ -19,3 +19,23 @@ export default function Clock({ time }) {
   }
   return <h1 id="time">{time.toLocaleTimeString()}</h1>;
 }
+
+// Solution
+
+/**
+ * export default function Clock({ time }) {
+  let hours = time.getHours();
+  let className;
+  if (hours >= 0 && hours <= 6) {
+    className = 'night';
+  } else {
+    className = 'day';
+  }
+  return (
+    <h1 className={className}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
+
+ */
