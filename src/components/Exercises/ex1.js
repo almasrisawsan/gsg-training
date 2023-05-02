6d979ac03da2eeaa22ac1f3b0570eb635b714a3d
Oways Asi <owayslfkf@gmail.com>
2023-05-02 18:12:07 +0300

    solution of day 8

--- src/components/Exercises/ex1.js
+++ src/components/Exercises/ex1.js
@@ -17,3 +17,25 @@ export default function LightSwitch() {
 
   return <button onClick={handleClick()}>Toggle the lights</button>;
 }
+
+// Solution
+
+/**
+ * export default function LightSwitch() {
+  function handleClick() {
+    let bodyStyle = document.body.style;
+    if (bodyStyle.backgroundColor === 'black') {
+      bodyStyle.backgroundColor = 'white';
+    } else {
+      bodyStyle.backgroundColor = 'black';
+    }
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Toggle the lights
+    </button>
+  );
+}
+
+ */
--- src/components/Exercises/ex2.js
+++ src/components/Exercises/ex2.js
@@ -43,3 +43,21 @@ export default function App() {
   );
 }
 */
+
+// Solution
+
+/**
+ * export default function ColorSwitch({
+  onChangeColor
+}) {
+  return (
+    <button onClick={e => {
+      e.stopPropagation();
+      onChangeColor();
+    }}>
+      Change color
+    </button>
+  );
+}
+
+ */
