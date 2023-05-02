30dd77378372d87acde5cb25783161e117196196
Oways Asi <owayslfkf@gmail.com>
2023-05-02 18:16:33 +0300

    solution of day 9

--- src/components/Exercises/ex6.js
+++ src/components/Exercises/ex6.js
@@ -17,3 +17,23 @@ export default function FeedbackForm() {
 
   return <button onClick={handleClick}>Greet</button>;
 }
+
+// Solution
+
+/**
+ * import { useState } from 'react';
+
+export default function FeedbackForm() {
+  function handleClick() {
+    const name = prompt('What is your name?');
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
+
+ */
